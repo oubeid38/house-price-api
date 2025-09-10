933d64314deb9cb66f80975440cf0f825cb53aa1
Oubeid <oubeid38@live.fr>
2025-09-10 10:02:28 +0200

    cors origins

--- api_housing.py
+++ api_housing.py
@@ -3,9 +3,14 @@ from fastapi import FastAPI  # Pour l'API
 from pydantic import BaseModel  # Pour valider les entrées
 import joblib  # Pour charger le modèle
 import pandas as pd  # Pour manipuler les données
+from fastapi.middleware.cors import CORSMiddleware
+ 
 
 # Créer l'application
 app = FastAPI(title="API Prédiction Prix Maisons")
+origins = ["https://housing-static-site.onrender.com/"]
+app.add_middleware(    CORSMiddleware,    allow_origins=origins,    allow_credentials=True,    allow_methods=["*"],    allow_headers=["*"],)
+ 
 
 # Définir le modèle d'entrée
 class HouseFeatures(BaseModel):
@@ -27,3 +32,4 @@ def predict_price(features: HouseFeatures):
     input_data = pd.DataFrame([features.dict()])  # Convertir en tableau
     predicted_price = model.predict(input_data)[0]  # Prédire
     return {"predicted_price": predicted_price}  # Renvoie en JSON
+
